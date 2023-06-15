97b104e4243b986241c3a59a16fb3dea63e53a8a
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-06-15 16:27:15 +0100

    Added more Scripts and C programs to the 0x00-hello_world directory

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Entry point of thid program that prints out a string
+ * Return: If 0 means it exited successfully
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Entry point of thid program that prints out a string using printf
+ * Return: If 0 means it exited successfully
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
