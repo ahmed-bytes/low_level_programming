40e1c3786f1872523f071dae27e5c6fd5d6c5067
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-06-16 20:50:19 +0100

    Added a source file to print the alphabets in lower letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point of this program
+ * Desc: Print all the alphabets in lowercase
+ * Return: 0 means exited successfully
+ */
+
+int main(void)
+{
+	int letter = 97;
+
+	while (letter <= 122)
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	return (0);
+}
