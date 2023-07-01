f63100c711717ddb48f047b70eff26d5342ca3b8
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-07-01 16:28:22 +0100

    Update 11-print_to_98.c source code to produce the required output in a structured way

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,6 +3,7 @@
 /**
  * print_to_98 - Entry Point of the Program
  * Desc: prints all natural numbers from n to 98
+ * @n: parameter to be passed
  * Return: 0 means ran and exited successfully
  */
 
@@ -13,6 +14,10 @@ void print_to_98(int n)
 		while (n >= 98)
 		{
 			printf("%d, ", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
 			n--;
 		}
 		printf("\n");
@@ -22,13 +27,16 @@ void print_to_98(int n)
 		while (n <= 98)
 		{
 			printf("%d, ", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
 			n++;
 		}
 		printf("\n");
 	}
 	else
 	{
-		printf("%d, ", n);
-		printf("\n");
+		printf("%d\n, ", n);
 	}
 }
