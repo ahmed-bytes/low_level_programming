aaa7e5763e057ccd0ea676f490a26d807bfe3558
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-07-01 17:13:26 +0100

    Started working on 0x04-more_functions_nested_loops directory and added some c source programs and my header files

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - A function that prints the numbers, from 0 to 9
+ * Return: returns 0 if successful
+ */
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num <= 10)
+	{
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef main_h
+#define main_h
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+#endif
