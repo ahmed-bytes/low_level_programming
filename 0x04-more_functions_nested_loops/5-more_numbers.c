1c5e1b23e154e68d9b49a032eee484e2bcfb2795
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-07-01 22:23:46 +0100

    Resolve a typo error to match the betty coding style

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_most_numbers - A function that prints the numbers, from 0 to 9
+ * print_numbers - A function that prints the numbers, from 0 to 9
  * Return: returns 0 if successful
  */
 
