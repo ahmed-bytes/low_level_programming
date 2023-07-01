0f8a390043cce1a4f7f49479432252316480ba95
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-07-01 16:20:23 +0100

    Remove trailing white spaces in 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,20 +1,16 @@
 #include "main.h"
 /**
  * times_table - Entry Point of the Function
- * desc: prints the 9 times table, starting with 0
  * Return: 0 means ran and exited successfully
  */
 void times_table(void)
 {
-	int num1;
-	int num2;
+	int num1, num2;
 
 	num1 = num2 = 0;
-	
 	while (num1 <= 9)
 	{
 		num2 = 0;
-		
 		while (num2 <= 9)
 		{
 			if ((num1 * num2) < 10)
@@ -24,7 +20,6 @@ void times_table(void)
 					_putchar(' ');
 				}
 				_putchar((num1 * num2) + '0');
-				
 				if (num2 != 9)
 				{
 					_putchar(',');
@@ -35,7 +30,6 @@ void times_table(void)
 			{
 				_putchar(((num1 * num2) / 10) + '0');
 				_putchar(((num1 * num2) % 10) + '0');
-				
 				if (num2 != 9)
 				{
 					_putchar(',');
