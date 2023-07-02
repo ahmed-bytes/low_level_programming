bfe7dd42b7e2cc34c9b071873ce0db07a4f829af
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-07-02 17:39:52 +0100

    Refactored the fizzbuzz source code to pass the checker

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,23 @@ int main(void)
 	{
 		if (number % 3 == 0 && number % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (number % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (number % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", number);
+			printf("%d", number);
+		}
+		if (number != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
