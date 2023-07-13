d2faa22dca791a7d076d1b14269368d871b51db0
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-07-13 19:34:20 +0100

    Added source files for the pointers, arrays and strings project in the 0x05-pointers_arrays_strings repo

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev -  function that prints a string followed by a new line in reverse
+ * @s: parameter for the string
+ */
+
+void print_rev(char *s)
+{
+	int increase_count = 0;
+	int decrease_count;
+
+	for (; *s != '\0'; )
+	{
+		increase_count++;
+		s++;
+	}
+	for (decrease_count = increase_count; decrease_count >= 0; decrease_count--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
