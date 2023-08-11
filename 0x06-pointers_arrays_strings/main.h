29dc872316c079f73d0f06bd83b212941a531371
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-08-11 22:17:25 +0100

    Added more c source files for the more pointer, arrays and strings project

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,16 +1,6 @@
 #ifndef main_h
 #define main_h
 int _putchar(char);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 #endif
