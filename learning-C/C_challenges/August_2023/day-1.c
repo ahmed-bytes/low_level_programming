a3d6627bbca9c31a12e94f1970d27238907d9e4b
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-08-29 23:58:55 +0100

    Added a few lines of code to the source file to describe the program

--- learning-C/C_challenges/August_2023/day-1.c
+++ learning-C/C_challenges/August_2023/day-1.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
 
 /**
-*
+* Main- The entry point of the program that finds
+* The Maximum, Minimum and Total of numbers inputted
+* Return: 0 means it ran and exited successfully
+*/
+
+int main(int argc, char *argv[])
+{
+
+  return EXIT_SUCCESS;
+}
