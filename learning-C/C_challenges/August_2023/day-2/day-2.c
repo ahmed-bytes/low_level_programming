7290284bd9d0ac1a013b4b5d381e3be15e4207cc
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-09-01 11:00:13 +0100

    Refactored the day2 source code

--- learning-C/C_challenges/August_2023/day-2/day-2.c
+++ learning-C/C_challenges/August_2023/day-2/day-2.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 /**
-* main- Entry point of this program that
+* main- Entry point of this program that most occuring element in an array
 * Return: 0 means the program ran and evited successfully
 */
+
+int main(void)
+{
+  int arr[], size;
+
+  printf("Enter the size of the numbers: ");
+  scanf("%d", &size);
+
+
+  return (0);
+}
