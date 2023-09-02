ae551dad0b8739627378ae2c7d5946dd5d1c6884
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-09-02 17:07:20 +0100

    Refactored and  fixed a bug at the loop in the day2 source code

--- learning-C/C_challenges/August_2023/day-2/day-2.c
+++ learning-C/C_challenges/August_2023/day-2/day-2.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 /**
-* main- Entry point of this program that most occuring element in an array
+* main- Entry point of this program that outputs the most occuring number
 * Return: 0 means the program ran and evited successfully
 */
 
 int main(void)
 {
-  int arr[], size;
+  int size;
 
   printf("Enter the size of the numbers: ");
   scanf("%d", &size);
+  int arr[size];
 
+  int check = 0;
+  while (check < size)
+  {
+    if (scanf("%d", &arr[check])== 1)
+    {
+      check++;
+    }
+    else 
+  {
+      break;
+    }
+  }
+
+  for (int i = 0; i < size; i++)
+  {
+    printf("%d", arr[i]);
+  }
+
+  printf("\n");
 
   return (0);
 }
