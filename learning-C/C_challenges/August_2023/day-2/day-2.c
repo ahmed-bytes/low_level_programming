b298b95c2aba04496a31d04f2bc86280a1e89752
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-09-03 23:47:37 +0100

    updated and  fixed a bug at the conditions in the day2 source code

--- learning-C/C_challenges/August_2023/day-2/day-2.c
+++ learning-C/C_challenges/August_2023/day-2/day-2.c
@@ -6,28 +6,64 @@
 
 int main(void)
 {
-  int size;
+  int size, check, count = 1, check2;
 
   printf("Enter the size of the numbers: ");
   scanf("%d", &size);
   int arr[size];
 
-  int check = 0;
+  printf("Enter numbers: ");
+
+  check = 0;
   while (check < size)
   {
-    if (scanf("%d", &arr[check])== 1)
+    if (scanf("%d", &arr[check]) == 1)
     {
       check++;
     }
-    else 
-  {
-      break;
+    else
+    {
+      printf("Enter a reasonable number\n");
     }
   }
 
-  for (int i = 0; i < size; i++)
+  check2 = arr[0];
+
+  for (int i = 1; i < size; i++)
   {
-    printf("%d", arr[i]);
+    if (check2 == arr[i])
+    {
+      count++;
+    }
+    else
+    {
+      count--;
+    }
+
+    if (count == 0)
+    {
+      check2 = arr[i];
+      count = 1;
+    }
+  }
+
+  count = 0;
+
+  for (int j = 0; j < size; j++)
+  {
+    if (arr[j] == check2)
+    {
+      count++;
+    }
+  }
+
+  if (count > size / 2)
+  {
+    printf("The Majority Element is: %d", check2);
+  }
+  else
+  {
+    printf("There's no majority element\n");
   }
 
   printf("\n");
