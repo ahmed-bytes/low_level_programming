b4ae6aad88014e3c4fc59853f1daeb10945b3cba
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-09-06 18:34:39 +0100

    updated the source code for day8 by correcting a bug at the loop

--- C_challenges/September_2023/day-8/day-8.c
+++ C_challenges/September_2023/day-8/day-8.c
@@ -7,15 +7,23 @@
 
 int main(void)
 {
-  int size;
+  int size, t1 = 0, t2 = 1;
+  int sum = t1 + t2;
 
   printf("Enter The number of Finobacci numbers to generate: ");
   scanf("%d", &size);
 
-  int i = 0;
-  while (i < size)
-  {
+  printf("The Finobacci series are: %d, %d, ", t1, t2);
 
+  for (int i = 3; i <= size; i++)
+  {
+    printf("%d, ", sum);
+    t1 = t2;
+    t2 = sum;
+    sum = t1 + t2;
   }
+
+  printf("\n");
+
   return (0);
 }
