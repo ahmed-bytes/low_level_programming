8b7dcafcd0128202d03118f06f522d9d219a1f24
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-09-22 02:48:49 +0100

    Added day 14, 15 and 16 task plus setup the cs50 lib

--- C_challenges/September_2023/day-14/day-14.c
+++ C_challenges/September_2023/day-14/day-14.c
@@ -1,17 +1,39 @@
 #include <stdio.h>
+#include <cs50.h>
 /**
  * main - The entry point of this program
  * desc:
  * Return: 0 means ran and exited successfully
  */
 
+typedef struct
+{
+  string name;
+  int votes;
+}
+candidate;
+
+
+candidate get_candidate(string prompt);
+
+
 int main(void)
 {
-	int name;
+  candidate president = get_candidate("Enter a candidate: ");
 
-	name = 5;
+  printf("Name of Candidate: %s\n", president.name);
+  printf("Candidate votes: %i\n", president.votes);
+  return 0;
+}
 
-	printf("The number is: %d\n");
 
-	return (0);
+candidate get_candidate(string prompt)
+{
+  printf("%s\n", prompt);
+
+  candidate c;
+  c.name = get_string("Enter a name: ");
+  c.votes = get_int("Enter votes: ");
+
+  return c;
 }
---
+++ C_challenges/September_2023/day-15/day-15.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+typedef struct
+{
+  string name;
+  int vote;
+}
+candidate;
+
+get_candidate(string prompt);
+
+int main(void)
+{
+
+  return 0;
+}
