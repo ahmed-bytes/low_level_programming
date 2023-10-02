79f9d0992f6a56059b5b5dcdb169dc3aede77210
Ahmed <oladapoahmed5@gmail.com>
2023-10-02 17:00:43 +0100

    Added Day-15 challenge

--- C_challenges/September_2023/day-15/day-15.c
+++ C_challenges/September_2023/day-15/day-15.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#include <cs50.h>
-
-typedef struct
-{
-  string name;
-  int vote;
-}
-candidate;
-
-get_candidate(string prompt);
+/**
+* main- Prints the memory address of some values in array
+* Return: 0 means success
+*/
 
 int main(void)
 {
+  int array[] = {1, 3, 4, 6, 4, 2, 4};
+  int *p = array;
+
+  printf("The value of %d in memory is %p\n", *p, p);
+  printf("The value of %d in memory is %p\n", *(p + 1), &(p + 1));
 
   return 0;
 }
